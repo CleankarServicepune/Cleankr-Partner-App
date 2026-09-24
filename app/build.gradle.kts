e9de2a41cff71ca08d3a3bc02bba1f845bb21539
CleankarServicepune <karegavemadhavs7385@gmail.com>
2026-09-24 14:39:17 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,7 +133,4 @@ dependencies {
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
-}
+  debugImplementation(libs.androidx
